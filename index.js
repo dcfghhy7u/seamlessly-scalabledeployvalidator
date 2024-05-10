855d1c10b89c43965e4bfb1473223bdc1d1cebbd
dcfghhy7u <gs1yongzile@hotmail.com>
2024-05-10 22:46:53 +0700

    feat project commit

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
-function removeDuplicates(nums) {
-  let index = 0;
-  for (let i = 1; i < nums.length; i++) {
-    if (nums[i] !== nums[index]) {
-      index++;
-      nums[index] = nums[i];
+function combinationSum3(k, n) {
+  const result = [];
+  backtrack([], 1, k, n);
+  return result;
+  function backtrack(combination, start, k, n) {
+    if (n === 0 && k === 0) {
+      result.push([...combination]);
+      return;
+    }
+    if (n < 0 || k === 0) return;
+    for (let i = start; i <= 9; i++) {
+      combination.push(i);
+      backtrack(combination, i + 1, k - 1, n - i);
+      combination.pop();
     }
   }
-  return index + 1;
 }
